cf6dc97979f59f57c09700bf6460508d5e6385c5
Mykola_Kh <hizhym@gmail.com>
2022-11-09 21:56:04 +0200

    3rd done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+import throttle from 'lodash.throttle';
 
+const formRef = document.querySelector('.feedback-form');
+formRef.addEventListener('input', throttle(onInput, 500));
+formRef.addEventListener('submit', onFormSubmit);
+const STORAGE_KEY = 'feedback-form-state';
+const data = {};
+
+FormToLoad();
+
+function onInput(e) {
+  data[e.target.name] = e.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(data));
+  console.log(data);
+}
+
+function FormToLoad() {
+  const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  if (savedData) {
+    const inputRef = formRef.querySelector('input');
+    const textareaRef = formRef.querySelector('textarea');
+    inputRef.value = savedData.email ? savedData.email : '';
+    textareaRef.value = savedData.message ? savedData.message : '';
+    data.email = savedData.email;
+    data.message = savedData.message;
+  }
+}
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  if (data.email && data.message) {
+    console.log(data);
+    data.email = '';
+    data.message = '';
+    e.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+  }
+}
