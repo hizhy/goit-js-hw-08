33f859997ef2c8270aea7966b88a569d223ae9f1
Mykola_Kh <hizhym@gmail.com>
2022-11-09 02:59:12 +0200

    2nd done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import vimeoPlayer from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new vimeoPlayer(iframe);
+
+setCurrentTime();
+
+player.on('timeupdate', throttle(playedTime, 1000));
+
+function playedTime(data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+}
+function setCurrentTime() {
+  const savedTime = localStorage.getItem('videoplayer-current-time');
+  if (savedTime) {
+    player.setCurrentTime(savedTime);
+  }
+}
